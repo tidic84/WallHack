273b14fba28f187c712d536f7303e86ed80b6b0c
Tidic <tidic84@gmail.com>
2021-12-31 00:51:38 +0100

    :D

--- js/credits.js
+++ js/credits.js
@@ -1,16 +1,12 @@
 const bouton = document.querySelector("#tidic");
 const audio = document.querySelector("audio");
-const test = document.querySelector(".test");
-console.log(test)
 
-console.log(bouton)
-console.log(audio)
 bouton.addEventListener("click", () => {
-if (audio.paused) {
-audio.volume = 0.2;
-audio.play();
+    if (audio.paused) {
+        audio.volume = 0.2;
+        audio.play();
 
-} else {
-audio.pause();
-}
+    } else {
+        audio.pause();
+    }
 });
