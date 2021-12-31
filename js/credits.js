1c3b7148dc4ceb015e8ac3337686185c712836f7
Tidic <tidic84@gmail.com>
2021-12-31 10:05:53 +0100

    alert box

--- js/credits.js
+++ js/credits.js
@@ -5,6 +5,7 @@ bouton.addEventListener("click", () => {
     if (audio.paused) {
         audio.volume = 0.2;
         audio.play();
+        alert("Il n'y a rien a voir de plus !");
 
     } else {
         audio.pause();
